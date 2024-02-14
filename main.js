ac5aabada2a2656818ac67c49a33715c8d041a2e
Pavan80096 <144262925+Pavan80096@users.noreply.github.com>
2024-02-14 18:21:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+    strings: ["Backend Developer", "Trainee Software Specialist", "Full stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
